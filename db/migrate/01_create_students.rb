05439aad9d1bb5de444efc733b50c375ed2f823e
Robert Salaiz <work.salaizrm@gmail.com>
2020-03-31 23:15:18 +0000

    Automatically backed up by Learn

--- db/migrate/01_create_students.rb
+++ db/migrate/01_create_students.rb
@@ -1,14 +1,11 @@
 class CreateStudents < ActiveRecord::Migration[5.1]
 
-  def self.create_table
-    sql = <<-SQL
-    CREATE TABLE IF NOT EXISTS students (
-      id INTEGER PRIMARY KEY,
-      name TEXT
-    )
-    SQL
-
-    ActiveRecord::Base.connection.execute(sql)
+  def change
+    create_table :students do |i|
+      i.string :name
+    end
   end
+
+
 
 end

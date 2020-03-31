1a3842e5ecc2654f79b7e9b520e86a9d3d40affc
Robert Salaiz <work.salaizrm@gmail.com>
2020-03-31 23:19:57 +0000

    Automatically backed up by Learn

--- db/migrate/02_add_grade_and_birthdate_to_students.rb
+++ db/migrate/02_add_grade_and_birthdate_to_students.rb
@@ -1,7 +1,10 @@
 class AddGradeAndBirthdateToStudents < ActiveRecord::Migration
 
   def change
-
+    add_column :students do |i|
+      i.string :grade
+      i.string :birthdate
+    end
   end
 
 end
